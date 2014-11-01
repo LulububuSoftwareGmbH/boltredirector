971dacf77cd926a92a0b7eac212286113c8b5e8c
Mike Anthony <code@foundrybusiness.co.za>
2014-11-01 08:00:53 +0200

    small fixes: init.php; license.md

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 
-// Redirector Extension 1.0.0 for Bolt
+// Redirector Extension 1.0.1 for Bolt
 // by Foundry Code / Mike Anthony
 // Minimum Bolt version: 2.0
 // http://code.foundrybusiness.co.za/bolt-redirector
